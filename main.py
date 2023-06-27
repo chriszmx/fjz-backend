7a2178bf68490be77af399a674fde37254594d56
Chris <c.r.zambito@gmail.com>
2023-06-27 19:16:40 -0400

    uri

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from databases import Database
 
-DATABASE_URL = "Your database URL here"  # Replace with your PostgreSQL database URL
+DATABASE_URL = "postgres://buahjlpraqcgfr:79acca9b13aee2e87aa1ec40ce28b316cffca26abfcf6271695aa070c9279759@ec2-3-92-151-217.compute-1.amazonaws.com:5432/d33u912bcdif9u"
 
 app = FastAPI()
 database = Database(DATABASE_URL)

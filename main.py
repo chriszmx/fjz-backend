35055666772f7bfe4e59d366af733f094146f46f
Chris <c.r.zambito@gmail.com>
2023-06-27 22:12:24 -0400

    testing again

--- main.py
+++ main.py
@@ -1,62 +1,59 @@
+# from dotenv import load_dotenv
 import os
 from fastapi import FastAPI
-from sqlalchemy import create_engine
-from models import Base
-from database import database
-from routers import router
+from databases import Database
 
+# load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+# DATABASE_URL = "postgresql://admin:password@localhost:5432/fjz_local_db"
 
 
 app = FastAPI()
+database = Database(DATABASE_URL)
+
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
 
-    # create table on startup if not exists
-    engine = create_engine(DATABASE_URL)
-    Base.metadata.create_all(engine)
 
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
 
-app.include_router(router)
+
+@app.get('/')
+async def root():
+    return {"message": 'Hello, world!'}
 
 
 
-
-
-
-# # from dotenv import load_dotenv
 # import os
 # from fastapi import FastAPI
-# from databases import Database
+# from sqlalchemy import create_engine
+# from models import Base
+# from database import database
+# from routers import router
 
-# # load_dotenv()
 
 # DATABASE_URL = os.getenv("DATABASE_URL")
-# # DATABASE_URL = "postgresql://admin:password@localhost:5432/fjz_local_db"
+# if DATABASE_URL.startswith("postgres://"):
+#     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 
 # app = FastAPI()
-# database = Database(DATABASE_URL)
-
 
 # @app.on_event("startup")
 # async def startup():
 #     await database.connect()
 
+#     # create table on startup if not exists
+#     engine = create_engine(DATABASE_URL)
+#     Base.metadata.create_all(engine)
 
 # @app.on_event("shutdown")
 # async def shutdown():
 #     await database.disconnect()
 
-
-# @app.get('/')
-# async def root():
-#     return {"message": 'Hello, world!'}
+# app.include_router(router)

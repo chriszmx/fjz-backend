978765fe7b8356fd6805ccf28d5168e68fede2be
Chris <c.r.zambito@gmail.com>
2023-06-27 20:02:02 -0400

    trying without db

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI
-from databases import Database
+# from databases import Database
 
-DATABASE_URL = "postgres://buahjlpraqcgfr:79acca9b13aee2e87aa1ec40ce28b316cffca26abfcf6271695aa070c9279759@ec2-3-92-151-217.compute-1.amazonaws.com:5432/d33u912bcdif9u"
+# DATABASE_URL = "postgres://buahjlpraqcgfr:79acca9b13aee2e87aa1ec40ce28b316cffca26abfcf6271695aa070c9279759@ec2-3-92-151-217.compute-1.amazonaws.com:5432/d33u912bcdif9u"
 
 app = FastAPI()
-database = Database(DATABASE_URL)
+# database = Database(DATABASE_URL)
 
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
+# @app.on_event("startup")
+# async def startup():
+#     await database.connect()
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await database.disconnect()
 
 
 @app.get('/')

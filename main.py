9ad305303eccf8e42dfe4658ae1a4afe8d91d631
Chris <c.r.zambito@gmail.com>
2023-06-27 20:15:17 -0400

    env

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+from dotenv import load_dotenv
+import os
 from fastapi import FastAPI
-# from databases import Database
+from databases import Database
 
-# DATABASE_URL = "postgres://buahjlpraqcgfr:79acca9b13aee2e87aa1ec40ce28b316cffca26abfcf6271695aa070c9279759@ec2-3-92-151-217.compute-1.amazonaws.com:5432/d33u912bcdif9u"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = FastAPI()
-# database = Database(DATABASE_URL)
+database = Database(DATABASE_URL)
 
 
-# @app.on_event("startup")
-# async def startup():
-#     await database.connect()
+@app.on_event("startup")
+async def startup():
+    await database.connect()
 
 
-# @app.on_event("shutdown")
-# async def shutdown():
-#     await database.disconnect()
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get('/')

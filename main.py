bb9f6c524cab0b63651533036272aaa7e5f7a2c4
Chris <c.r.zambito@gmail.com>
2023-06-27 21:12:38 -0400

    test

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-# from dotenv import load_dotenv
-import os
-from fastapi import FastAPI
-from databases import Database
-
-# load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from . import models, crud, database
 
 app = FastAPI()
-database = Database(DATABASE_URL)
+models.Base.metadata.create_all(bind=database.engine)
 
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-
-@app.get('/')
-async def root():
+@app.get("/")
+def root():
     return {"message": "Hello, world!"}
 
-@app.get("/users")
-async def read_users():
-    query = "SELECT * FROM users"
-    results = await database.fetch_all(query)
-    return {"users": results}
+
+@app.get("/items")
+def read_items(db: Session = Depends(get_db)):
+    items = crud.get_items(db)
+    return {"items": items}
+
+
+@app.post("/items")
+def create_item(name: str, description: str, db: Session = Depends(get_db)):
+    item = crud.create_item(db, name, description)
+    return {"item": item}
